a79b623745767ab042e6bb1f116e5daf26497062
Xanthorn <59312494+Xanthorn@users.noreply.github.com>
2021-10-21 18:15:56 +0200

    Add finished function for exercise 2.3a

--- Exercise_2/exercise2.js
+++ Exercise_2/exercise2.js
@@ -1,3 +1,6 @@
+let exercise2_3_array = [];
+let exercise2_3_sum = 0;
+
 function exercise2_1() {
     let output = '';
     for(let i = 1; i <= 100; i++) {
@@ -25,4 +28,22 @@ function exercise2_2() {
     surfaceArea = Math.round(Math.PI * radius * radius * 100) / 100; // you need to multiply by 100 and then divide by 100
 
     console.log(`The circumference is equal to: ${circumference} and surface area is equal to: ${surfaceArea}`);
+}
+
+function exercise2_3_printAll() {
+    console.log(exercise2_3_array);
+}
+
+function exercise2_3a() {
+    exercise2_3_sum = 0;
+    exercise2_3_array = [];
+    min = Math.ceil(1);
+    max = Math.floor(10);
+    while (exercise2_3_sum < 200) {
+        number = Math.floor(Math.random() * (max - min)) + min;
+        exercise2_3_sum += number;
+        exercise2_3_array.push(number);
+    }
+    
+    exercise2_3_printAll();
 }
