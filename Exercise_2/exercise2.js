4bdaaf48f51f1650b6cbc68e648388ad53e9ca71
Xanthorn <59312494+Xanthorn@users.noreply.github.com>
2021-10-21 17:23:58 +0200

    Add finished exercise 2.2

--- Exercise_2/exercise2.js
+++ Exercise_2/exercise2.js
@@ -19,6 +19,10 @@ function exercise2_1() {
 function exercise2_2() {
     let radius = 0, circumference = 0.0, surfaceArea = 0.0;
 
-    radius = window.prompt("Podaj promień");
+    radius = window.prompt("Enter the radius");
 
+    circumference = Math.round(2 * Math.PI * radius * 100) / 100;    // round() round the number to integer so if you want to get 2 places after comma then 
+    surfaceArea = Math.round(Math.PI * radius * radius * 100) / 100; // you need to multiply by 100 and then divide by 100
+
+    console.log(`The circumference is equal to: ${circumference} and surface area is equal to: ${surfaceArea}`);
 }
