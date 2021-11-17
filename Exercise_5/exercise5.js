727532621053079f93aa89b567951b25bb01fd40
Xanthorn <59312494+Xanthorn@users.noreply.github.com>
2021-11-18 00:18:33 +0100

    Add function adding length to show in <p> title

--- Exercise_5/exercise5.js
+++ Exercise_5/exercise5.js
@@ -1,13 +1,25 @@
 const textColors = ["#ff0000", "#0040ff", "#ff00ff", "#00ff00"]
 
-function setTextColorOnLoad() {
+function runOnLoad() {
     let paragraphs = document.getElementsByTagName("p");
 
+    setTextColorOnLoad(paragraphs);
+    addTitlesOnLoad(paragraphs);
+}
+
+function setTextColorOnLoad(paragraphs) {
     min = Math.ceil(0);
     max = Math.floor(paragraphs.length - 1);
 
     for(let i = 0; i < paragraphs.length; i++) {
         let colorIndex = Math.floor(Math.random() * (max - min + 1)) + min;
         paragraphs[i].style.color = textColors[colorIndex];
+    }
+}
+
+function addTitlesOnLoad(paragraphs) {
+    for(let i = 0; i < paragraphs.length; i++) {
+        let paragraphLength = paragraphs[i].textContent.length;
+        paragraphs[i].title = `Długość paragrafu: ${paragraphLength}`;
     }
 }
