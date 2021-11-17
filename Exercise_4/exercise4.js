7f5a95708796fda93cad4ca624f2a9c6151fb5ea
Xanthorn <59312494+Xanthorn@users.noreply.github.com>
2021-11-17 22:27:48 +0100

    Add Products List class to exercise 4

--- Exercise_4/exercise4.js
+++ Exercise_4/exercise4.js
@@ -35,23 +35,55 @@ class Product {
 
 class ProductsList {
     constructor() {
-        this.products = new [];
+        this.list = new Array();
     }
 
-    PrintProducts() {
-        if(this.products.length() === 0) {
-            console.log("List of products is empty");
+    logAll() {
+        if (this.list.length === 0) {
+            console.log("The products list is empty");
+        } else {
+            console.log(this.list);
         }
-        else {
-            for(let i = 0; i < this.products.length(); i++) {
-                console.log(`${this.products[i]}\n`);
+    };
+
+    log(id) {
+        if (this.list.length === 0) {
+            console.log("The products list is empty");
+            return;
+        } else {
+            for (let i = 0; i < this.list.length; i++) {
+                if (this.list[i].id === id) {
+                    console.log(this.list[i]);
+                    return;
+                }
             }
         }
-    }
 
-    PrintProduct(id) {
-        
-    }
+        console.log("There is no product with given id in product list");
+    };
+
+    addProduct(product) {
+        for (let i = 0; i < this.list.length; i++) {
+            if (product.id === this.list[i].id) {
+                console.log("Product with given id is already in product list");
+                return;
+            }
+        }
+
+        this.list.push(product);
+    };
+
+    modifyProduct(id, product) {
+        for (let i = 0; i < this.list.length; i++) {
+            if (id === this.list[i].id) {
+                this.list[i] = product;
+                console.log("Changes has been made successfully");
+                return;
+            }
+        }
+
+        console.log("There is no product with given id in product list");
+    };
 }
 
 let product = new Product(1, "PC", "Gaming", 2020, 5500, 0.6);
@@ -61,3 +93,20 @@ console.log(product.Price);
 console.log(product.EnergyBill);
 console.log(product.AgeOfProduct);
 console.log(product.AgeOfProductInYears);
+
+let productsList = new ProductsList();
+
+/////////////////////////////////////////
+console.log("\n\n");
+/////////////////////////////////////////
+
+productsList.logAll();
+
+productsList.addProduct(product);
+productsList.logAll();
+productsList.log(1);
+productsList.log(2);
+
+modifiedProduct = new Product(1, "PC", "Gaming", 2020, 6000, 0.4);
+productsList.modifyProduct(modifiedProduct.id, modifiedProduct);
+productsList.log(1);
